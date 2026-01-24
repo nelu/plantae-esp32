3474e05d37aeffac6566de2139d239b22e11ca75
nelu <nelu@github.com>
2026-01-24 09:47:16 +0200

    fix reboot to close all pwm debugs everywhere bootstrapping network in boot before msgpack

--- src/drivers/pwm_out.py
+++ src/drivers/pwm_out.py
@@ -7,9 +7,24 @@ class PwmOut:
         self.set(0.0)
 
     def set(self, duty01):
+        pwm = self._pwm
         if duty01 < 0: duty01 = 0
         if duty01 > 1: duty01 = 1
         v = int(duty01 * 65535)
         if self.active_low:
             v = 65535 - v
-        self._pwm.duty_u16(v)
+        pwm.duty_u16(v)
+
+    def release(self):
+        pwm = self._pwm
+        if not pwm:
+            return
+        try:
+            pwm.duty_u16(0)
+        except Exception:
+            pass
+        try:
+            pwm.deinit()
+        except Exception:
+            pass
+        self._pwm = None
